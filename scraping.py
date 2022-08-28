4fe8432119f53a89f05611ce9e92ad4b95229435
Rei Sasajima <sasajimarei@gmail.com>
2022-08-28 21:13:58 +0900

    push python files to repository

---
+++ scraping.py
@@ -0,0 +1,94 @@
+import idna
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import os, signal
+import csv
+
+# chrome_options = Options()
+# chrome_options.add_experimental_option("detach", True)
+
+# 下のoption addargumentでブラウザ非表示で実行
+options = Options()
+options.add_argument('--headless')
+# chromeoption=optionsでブラウザ非表示を適用
+driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
+driver.implicitly_wait(10)
+# driver.get('https://aoyama-portal.aoyama.ac.jp/aogaku_auth/jsp/AUTH01.jsp?TYPE=33554433&REALMOID=06-2a27689c-587a-42e0-834c-7a6751c24219&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-y%2fO%2bwXHmQFuKEoGlz9CdfIzo1Ujfp%2fcsgj66pcDn%2bqoJ8OCObeoRcIMe463sPxZH&TARGET=-SM-https%3a%2f%2faoyama--portal%2eaoyama%2eac%2ejp%2fprotect%2f')
+
+driver.get('http://syllabus.aoyama.ac.jp/')
+# driver.get('https://aoyama-portal.aoyama.ac.jp/aogaku_auth/jsp/AUTH01.jsp?TYPE=33554433&REALMOID=06-2a27689c-587a-42e0-834c-7a6751c24219&GUID=&SMAUTHREASON=0&METHOD=GET&SMAGENTNAME=-SM-y%2fO%2bwXHmQFuKEoGlz9CdfIzo1Ujfp%2fcsgj66pcDn%2bqoJ8OCObeoRcIMe463sPxZH&TARGET=-SM-https%3a%2f%2faoyama--portal%2eaoyama%2eac%2ejp%2fprotect%2f')
+
+# 授業名の項目
+# courseTitle = driver.find_element_by_id('CPH1_KM')
+# courseTitle.send_keys("")
+
+# 大学キャンパスの選択(下は青山を選択)
+campus = driver.find_element_by_id('CPH1_rptCP_CP_0')
+campus.click()
+
+# 講師名の項目
+# lecturer = driver.find_element_by_id('CPH1_KI')
+# lecturer.send_keys("名前")
+
+# 検索ボタン
+search = driver.find_element_by_id('CPH1_btnKensaku')
+search.click()
+
+
+for i in range(0, 20):
+  dateTime = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblJigen_{i}')
+  title = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKamoku_{i}')
+  lecturerName = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKyouin_{i}')
+
+  # courseDescription = driver.find_element_by_id('CPH1_lblGaiyou')
+  # driver.back()
+
+  print(dateTime.text, '授業名:', title.text, '講師名:', lecturerName.text)
+
+
+  # print(courseDescription.text)
+
+# 詳細へアクセス
+
+# 次のページへのボタン
+nextBtn1 = driver.find_element_by_id('CPH1_rptPagerB_lnkPage_1')
+nextBtn1.click()
+
+
+for i in range(0, 20):
+  dateTime = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblJigen_{i}')
+  title = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKamoku_{i}')
+  lecturerName = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKyouin_{i}')
+  print(dateTime.text, '授業名:', title.text, '講師名:', lecturerName.text)
+
+
+
+
+# for j in range(1, 380):
+#   for i in range(0, 20):
+#     dateTime = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblJigen_{i}')
+#     title = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKamoku_{i}')
+#     lecturerName = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKyouin_{i}')
+#     print(dateTime.text)
+#     print(title.text)
+#     print(lecturerName.text)
+  
+#   nextBtn(j) = driver.find_element_by_id('CPH1_rptPagerB_lnkPage_{j}')
+#   nextBtn(j).click()
+
+#   for i in range(0, 20):
+#     dateTime = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblJigen_{i}')
+#     title = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKamoku_{i}')
+#     lecturerName = driver.find_element_by_id(f'CPH1_gvw_kensaku_lblKyouin_{i}')
+#     print(dateTime.text)
+#     print(title.text)
+#     print(lecturerName.text)
+
+
+
+
+# chromeを開いたままpython seleniumを終了してchromeを開いたままにする
+os.kill(driver.service.process.pid, signal.SIGTERM)
+
+
